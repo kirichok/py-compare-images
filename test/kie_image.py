2caf67dbebf20a7bb26fc32315e3b3fe97d66b26
kirichok <evgeniyk@tcgsi.com>
2017-04-12 09:09:46 +0300

    PY 3.6

--- test/kie_image.py
+++ test/kie_image.py
@@ -2,7 +2,7 @@ from __future__ import division
 import numpy as np
 import cv2
 # from matplotlib import pyplot as plt
-import urllib
+import urllib.request as urllib
 import math
 from os.path import basename, splitext
 import _pickle as cPickle
