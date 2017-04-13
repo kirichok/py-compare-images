1bc9b64ef8f12ccef9494d8d350bff7307a1818b
kirichok <evgeniyk@tcgsi.com>
2017-04-13 11:11:15 +0300

    check 2.7

--- test/kie_find_by_hash.py
+++ test/kie_find_by_hash.py
@@ -3,101 +3,127 @@ import threading
 import cv2
 import glob
 import numpy as np
-import time
-# import mysql_conn
-
-import kie_mysql as sql
 import kie_image as image
+import argparse
+import os
 
 HASH_PATH = '../images/hash/'
-KP_EXT = '.kp'
-DES_EXT = '.des.jpg'
+DES_EXT = '.jpg.des'
 
-THREAD_COUNT = 200
 exitFlag = 0
 
 class HashThread(threading.Thread):
-    def __init__(self, threadID, name, q):
+    def __init__(self, threadID, name, lock, workQueue, kp, des, finds):
         threading.Thread.__init__(self)
         self.threadID = threadID
         self.name = name
-        self.q = q
+        self.workQueue = workQueue
+        self.lock = lock
+        self.kp = kp
+        self.des = des
+        self.finds = finds
 
     def run(self):
-        print "Starting " + self.name
-        queueLock.acquire()
-        des1 = des.copy()
-        queueLock.release()
+        global exitFlag
+        # print("Starting " + self.name)
         while not exitFlag:
-            queueLock.acquire()
-            if not workQueue.empty():
-                path = self.q.get()
-                queueLock.release()
+            self.lock.acquire()
+            if not self.workQueue.empty():
+                path = self.workQueue.get()
+                self.lock.release()
 
                 des2 = image.loadImageFromPath(path, cv2.IMREAD_GRAYSCALE, False)
                 des2 = np.asarray(des2, np.float32)
-                name = image.fileName(path)
                 if isinstance(des2, list) and len(des2) >= 2:
-                    m = image.match(des1, des2)
+                    name = image.fileName(path)
+                    m = image.match(self.des, des2)
                     if len(m) >= 50:
-                        print "Matched %s file %s" % (len(m), name)
+                        self.lock.acquire()
+                        self.finds.append({'m': len(m), 'n': name})
+                        self.lock.release()
+                        print("Matched %s file %s" % (len(m), name))
             else:
-                queueLock.release()
+                self.lock.release()
 
-        print "Exiting " + self.name
-
-img = image.loadImageFromPath('../images/z1.jpg', cv2.IMREAD_GRAYSCALE, True, 200)
-kp, des = image.getKpDes(img)
-
-threadList = []
-count = 0
-while count < THREAD_COUNT:
-    threadList.append("Thread-%d" % count)
-    count += 1
-
-nameList = []
-for imagePath in glob.glob("../images/hash/*.des.jpg"):
-    nameList.append(imagePath)
+        # print("Exiting " + self.name)
 
 
-if len(nameList) == 0:
-    exit(0)
-else:
-    print "Files count: %d " % len(nameList)
+def check(imgPath, hashPath=HASH_PATH, withSubFolders=True, threadsCount=200):
+    global exitFlag
 
-queueLock = threading.Lock()
-workQueue = Queue.Queue(1000000)
-threads = []
-threadID = 1
+    finds = []
+    exitFlag = 0
+    img = image.loadImageFromPath(imgPath, cv2.IMREAD_GRAYSCALE, True, 200)
+    kp, des = image.getKpDes(img)
 
-# Create new threads
-for tName in threadList:
-    thread = HashThread(threadID, tName, workQueue)
-    thread.daemon = True
-    thread.start()
-    threads.append(thread)
-    threadID += 1
+    threadList = []
+    count = 0
+    while count < threadsCount:
+        threadList.append("Thread-%d" % count)
+        count += 1
 
-# Fill the queue
-queueLock.acquire()
-for word in nameList:
-    workQueue.put(word)
-queueLock.release()
+    nameList = []
+    if withSubFolders:
+        folder = 1
+        path = "%s%s/" % (hashPath, folder)
+        while os.path.exists(path):
+            for imagePath in glob.glob("%s*%s" % (path, DES_EXT)):
+                nameList.append(imagePath)
+            folder += 1
+            path = "%s%s/" % (hashPath, folder)
+    else:
+        for imagePath in glob.glob("%s*%s" % (hashPath, DES_EXT)):
+            nameList.append(imagePath)
 
-e1 = cv2.getTickCount()
-# Wait for queue to empty
-while not workQueue.empty():
-    pass
+    if len(nameList) == 0:
+        print("Hash files count is empty")
+    else:
+        print("Files count: %d " % len(nameList))
 
-# Notify threads it's time to exit
-exitFlag = 1
+        queueLock = threading.Lock()
+        workQueue = Queue.Queue(1000000)
+        threads = []
+        threadID = 1
 
-# Wait for all threads to complete
-for t in threads:
-    t.join()
+        # Create new threads
+        for tName in threadList:
+            thread = HashThread(threadID, tName, queueLock, workQueue, kp, des, finds)
+            thread.start()
+            threads.append(thread)
+            threadID += 1
 
-e2 = cv2.getTickCount()
-time = (e2 - e1) / cv2.getTickFrequency()
-print "Time: %s s" % (time)
+        # Fill the queue
+        queueLock.acquire()
+        for word in nameList:
+            workQueue.put(word)
+        queueLock.release()
 
-print "Exiting Main Thread"
+        # Wait for queue to empty
+        while not workQueue.empty():
+            pass
+
+        # Notify threads it's time to exit
+        exitFlag = 1
+
+        # Wait for all threads to complete
+        for t in threads:
+            t.join()
+
+        return finds
+
+
+if __name__ == '__main__':
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-t", "--threads", required=True, type=int, help="Threads count", default=50)
+    ap.add_argument("-i", "--image", required=True, help="Path to the image")
+    ap.add_argument("-sf", "--subfolder", required=True, type=bool, help="Check sub folder", default=True)
+    ap.add_argument("-hf", "--hash", required=True, help="Path to the hash folder", default=HASH_PATH)
+    args = vars(ap.parse_args())
+
+    e1 = cv2.getTickCount()
+    f = check(args['image'], args['hash'], args['subfolder'], args['threads'])
+    e2 = cv2.getTickCount()
+    time = (e2 - e1) / cv2.getTickFrequency()
+    print("Time: %s s" % (time))
+    print("Finds: %s" % f)
+    print("Exiting Main Thread")
